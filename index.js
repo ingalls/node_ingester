ae41b8c0dd866cb4be497dd9ddfdb588a8335217
ingalls <nick@mapbox.com>
2014-12-16 10:19:09 -0500

    Gen Overpass query

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var readline = require('readline');
 var turf = require('turf');
 var cover = require('tile-cover');
 
+var overpass = "http://overpass-api.de/api/interpreter?data=";
+
 var argv = require('minimist')(process.argv, {
     'string': ['input', 'lat', 'lon', 'help'],
     'integer': ['tol'],
@@ -18,7 +20,7 @@ if (!argv.lat) throw new Error('--x argument required');
 if (!argv.lon) throw new Error('--y argument required');
 
 var tol = argv.tol ? argv.tol : 50;
-var tag = argv.signals ? "highway:traffic_signals" : "highway:stop";
+var tag = argv.signals ? { key: "highway", value: "traffic_signals" } : { key: "highway", value: "stop" };
 var head = true;
 var fileInput = fs.createReadStream(argv.input);
 var fileOutput = fs.createWriteStream('./out.csv');
@@ -42,9 +44,20 @@ rl.on('line', function (line) {
     } 
 });
 
-rl.on('close', function () {
+rl.on('close', getOSM);
+
+var query = "[out:json][timeout:25];(node[" + tag.key +  "=" + tag.value + "]({{bbox}}););out body;>;out skel qt;"
+
+function getOSM() {
+    
     fc = turf.featurecollection(collection);
     envelope = turf.envelope(fc);
-    tiles = cover.geojson(envelope.geometry, { min_zoom: 6, max_zoom: 6 });
+    tiles = cover.geojson(envelope.geometry, { min_zoom: 6, max_zoom: 6 }); 
     console.log(tiles.features.length);
-});
+
+    tiles.features.forEach(function(feat) {
+        var bbox = turf.extent(feat);
+        var query = overpass + encodeURIComponent("[out:json][timeout:25];(node[" + tag.key +  "=" + tag.value + "](" + bbox + "););out body;>;out skel qt;");
+        console.log(query);
+    });
+}
