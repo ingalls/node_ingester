27bb741ad161f24f087488439defd5edab6d98c7
ingalls <nick@mapbox.com>
2014-12-17 14:37:11 -0500

    Move to json param format

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ var argv = require('minimist')(process.argv, {
     'string': ['input', 'lat', 'lon', 'help', 'tag'],
     'integer': ['tol'],
 });
-if (argv.help || !argv.input || !argv.lat || !argv.lon || !argv.tag) {
-    console.log('./index.js --input FILE.csv --lat COL --lon COL --tag KEY:VALUE');
+
+var input = {};
+
+if (argv._.length === 3) {
+    input = require(argv._[2]);
+    input.tol = argv.tol ? argv.tol : 0.100;
+} else if (argv.help || !argv.input || !argv.lat || !argv.lon || !argv.tag) {
+    console.log('./index.js --input CSV|HTTP --lat COL --lon COL --tag KEY:VALUE [--tol TOL]');
+    console.log('./index.js JSON [--tol TOL]');
     process.exit();
+} else {
+    input = {
+        "lon": argv.lon,
+        "lat": argv.lat,
+        "input": argv.input,
+        "tag": argv.tag,
+        "tol": argv.tol ? argv.tol : 0.100
+    };
 }
 
 var request = require('request'),
@@ -16,47 +31,57 @@ var request = require('request'),
     async = require('async'),
     stream = require('stream');
 
+var tag =  { key: input.tag.split(':')[0], value: input.tag.split(':')[1]};
 var overpass = "http://overpass-api.de/api/interpreter?data=";
+var query = "[out:json][timeout:25];(node[" + tag.key +  "=" + tag.value + "]({{bbox}}););out body;>;out skel qt;";
 
-var tol = argv.tol ? argv.tol : 0.100; //In km
-var tag =  { key: argv.tag.split(':')[0], value: argv.tag.split(':')[1]};
-var head = true;
-var fileInput = fs.createReadStream(argv.input);
-var lon, lat,
-    collection = [],
+var collection = [],
     osmcollection = [];
 var fc, osmfc;
 
-var rl = readline.createInterface({
-    input: fileInput, 
-    output: new stream()
-});
+if (input.input.indexOf('http') !== -1) {
+    var output = fs.createWriteStream('/tmp/ingester.csv');
+    request(input.input).pipe(output);
+    output.on('close', function () {
+        input.input="/tmp/ingester.csv";
+        getData();
+    });
+} else getData();
 
-rl.on('line', function (line) {
-    if (head) {
-        line.split(',').forEach(function(col, i) {
-            if (argv.lon.toLowerCase() === col.toLowerCase()) lon = i;
-            else if (argv.lat.toLowerCase() === col.toLowerCase()) lat = i;
-        });
-        if (!lat || !lon) throw new Error('Could not determine lat/lon cols');
-        head = !head;
-    } else {
-        var lonRow = parseFloat(line.split(',')[lon]),
-            latRow = parseFloat(line.split(',')[lat]);
-        if (lonRow > -180 || lonRow < 180 || latRow > -85 || latRow < 85 || (lonRow !== 0 && latRow !== 0)) 
-            collection.push(turf.point(lonRow, latRow));
-        else console.log("Invalid GEOM skipped");
-    } 
-});
+function getData() {
+    var fileInput = fs.createReadStream(input.input);
+    var lon, lat;
+    var head = true;
 
-rl.on('close', getOSM);
+    var rl = readline.createInterface({
+        input: fileInput,
+        output: new stream()
+    });
 
-var query = "[out:json][timeout:25];(node[" + tag.key +  "=" + tag.value + "]({{bbox}}););out body;>;out skel qt;"
+    rl.on('line', function (line) {
+        if (head) {
+            line.split(',').forEach(function(col, i) {
+                if (input.lon.toLowerCase() === col.toLowerCase()) lon = i;
+                else if (input.lat.toLowerCase() === col.toLowerCase()) lat = i;
+            });
+            if (!lat || !lon) throw new Error('Could not determine lat/lon cols');
+            head = !head;
+        } else {
+            var lonRow = parseFloat(line.split(',')[lon]),
+                latRow = parseFloat(line.split(',')[lat]);
+            if (lonRow > -180 || lonRow < 180 || latRow > -85 || latRow < 85 || (lonRow !== 0 && latRow !== 0))
+                collection.push(turf.point(lonRow, latRow));
+            else console.log("Invalid GEOM skipped");
+        }
+    });
 
-function getOSM() { 
+    rl.on('close', getOSM);
+}
+
+function getOSM() {
     fc = turf.featurecollection(collection);
     envelope = turf.envelope(fc);
-    tiles = cover.geojson(envelope.geometry, { min_zoom: 7, max_zoom: 7 }); 
+    tiles = cover.geojson(envelope.geometry, { min_zoom: 7, max_zoom: 7 });
     var queries = [];
     async.each(tiles.features, function(feat, cb) {
         var bbox = turf.extent(feat);
@@ -65,7 +90,7 @@ function getOSM() {
             if (err || res.statusCode !== 200) cb(new Error("Overpass Query Failed!\n" + body));
             JSON.parse(body).elements.forEach(function(osmfeat) {
                 osmcollection.push(turf.point(osmfeat.lon, osmfeat.lat));
-            }); 
+            });
             setTimeout(cb, 1000); //Respect Overpass limits
         });
     }, function(err) {
@@ -76,10 +101,10 @@ function getOSM() {
 
 function diff() {
     osmfc = turf.featurecollection(osmcollection);
-    console.log("LON,LAT"); 
+    console.log("LON,LAT");
     fc.features.forEach(function(pt) {
         var nearest = turf.nearest(pt, osmfc);
-        if (turf.distance(pt, nearest, "kilometers") > tol)
+        if (turf.distance(pt, nearest, "kilometers") > input.tol)
             console.log(pt.geometry.coordinates.join(','));
     });
 }
